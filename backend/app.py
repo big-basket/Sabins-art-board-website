1cfab2e5b816ae6b4f6ada383cb13731e53770c4
big-basket <78880753+big-basket@users.noreply.github.com>
2025-02-17 20:00:59 +0000

    did a little fixaroo cause the files were being created in the wrong place

--- backend/app.py
+++ backend/app.py
@@ -6,14 +6,18 @@ from routes.upload_route import upload_file
 
 # Initializing flask app    
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(BASE_DIR, 'database', 'database.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER_IMAGES'] = 'uploads/images/'  
-app.config['UPLOAD_FOLDER_MARKDOWN'] = 'uploads/markdown/'  
+app.config['UPLOAD_FOLDER_IMAGES'] = 'backend/uploads/images/'  
+app.config['UPLOAD_FOLDER_MARKDOWN'] = 'backend/uploads/markdown/'  
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif', 'md'}
 
 os.makedirs(app.config['UPLOAD_FOLDER_IMAGES'], exist_ok=True)
 os.makedirs(app.config['UPLOAD_FOLDER_MARKDOWN'], exist_ok=True)
+os.makedirs(os.path.join(BASE_DIR, 'database'), exist_ok=True)
+
+
 
 db.init_app(app)
 
